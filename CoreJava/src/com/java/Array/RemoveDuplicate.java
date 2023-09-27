5a5e950378110de8306d3d4bef4ea7b9fe576c6a
Nadir-KT <65030168+Nadir-KT@users.noreply.github.com>
2023-09-27 20:36:05 +0530

    IDE

---
+++ CoreJava/src/com/java/Array/RemoveDuplicate.java
@@ -0,0 +1,22 @@
+package com.java.Array;
+import java.util.*;
+public class RemoveDuplicate {
+	  public static void main(String[] args) {
+
+	    // create an arraylist from the array
+	    // using asList() method of the Arrays class
+	    ArrayList<Integer> numbers = new ArrayList<>(Arrays.asList(1, 2, 3, 4, 1, 3));
+	    System.out.println("ArrayList with duplicate elements: " + numbers);
+
+	    // convert the arraylist into a set
+	    Set<Integer> set = new LinkedHashSet<>();
+	    set.addAll(numbers);
+
+	    // delete al elements of arraylist
+	    numbers.clear();
+
+	    // add element from set to arraylist
+	    numbers.addAll(set);
+	    System.out.println("ArrayList without duplicate elements: " + numbers);
+	  }
+	}
