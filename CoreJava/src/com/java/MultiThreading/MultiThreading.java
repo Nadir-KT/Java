98c8d2332ee527d36a961b657149ab1ca69f19bf
Nadir-KT <65030168+Nadir-KT@users.noreply.github.com>
2023-11-25 12:04:47 +0530

    Multithreading

---
+++ CoreJava/src/com/java/MultiThreading/MultiThreading.java
@@ -0,0 +1,12 @@
+package com.java.MultiThreading;//4:28
+
+public class MultiThreading {
+
+	public static void main(String[] args) {
+		/* MultiThreading: Executing multiple programs concurrently
+		 * Thread: Independent path of execution
+		 */
+
+	}
+
+}
