19724fe3e5f238fadff0141a853d92fc8a67263a
Nadir-KT <65030168+Nadir-KT@users.noreply.github.com>
2023-12-01 15:28:05 +0530

    IDE

---
+++ CoreJava/src/com/java/GUI/AWT/Canv.java
@@ -0,0 +1,16 @@
+package com.java.GUI.AWT;
+
+import java.awt.Frame;
+
+public class Canv {
+	//Canvas: Rectangular Area
+	public Canv() {
+		Frame f=new Frame("Canvas");
+		Canvas c=new Canvas();
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
