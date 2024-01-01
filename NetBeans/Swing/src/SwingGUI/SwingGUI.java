a1e8b99ab73a950cc4908b3e6139da10c943ec22
Nadir-KT <65030168+Nadir-KT@users.noreply.github.com>
2024-01-01 17:45:14 +0530

    SwingGUI

--- NetBeans/Swing/src/SwingGUI/SwingGUI.java
+++ NetBeans/Swing/src/SwingGUI/SwingGUI.java
@@ -2,6 +2,11 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  * Click https://netbeans.apache.org/tutorial/main/kb/docs/java/quickstart-gui/ for tutorials 
+ * Click https://netbeans.apache.org/tutorial/main/kb/docs/java/gbcustomizer-advanced/ for tutorials 
+ * Click https://netbeans.apache.org/tutorial/main/kb/docs/java/gbcustomizer-basic/ for tutorials 
+ * Click https://netbeans.apache.org/tutorial/main/kb/docs/java/gui-binding/ for tutorials 
+ * Click https://netbeans.apache.org/tutorial/main/kb/docs/java/gui-gaps/ for tutorials 
+ * Click https://www3.ntu.edu.sg/home/ehchua/programming/howto/netbeans_howto.html for tutorials 
  */
 package SwingGUI;
 
